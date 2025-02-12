a6974f433b1e007fe688b54554adf222bb5b509c
LAB13_USER ON WS15 <L13X13W15@citulab.net>
2025-02-12 12:38:45 +0800

    Added Backend Folder

---
+++ backend/src/main/kotlin/edu/cit/barangay360/BackendApplication.kt
@@ -0,0 +1,11 @@
+package edu.cit.barangay360
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BackendApplication
+
+fun main(args: Array<String>) {
+	runApplication<BackendApplication>(*args)
+}
---
+++ backend/src/test/kotlin/edu/cit/barangay360/BackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package edu.cit.barangay360
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
