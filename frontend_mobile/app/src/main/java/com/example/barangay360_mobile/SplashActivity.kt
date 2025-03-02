ebd129a0cda98ad695c5e589af9753cf15919134
MikhailAg0ny <mikhailjpn@gmail.com>
2025-03-03 01:14:12 +0800

    Added Splash Screen With Logo and Name V1
    -As using the JPG to insert the image "not good quality" but need changes later on
    -1:13 AM
    -March 3, 2025

---
+++ frontend_mobile/app/src/main/java/com/example/barangay360_mobile/SplashActivity.kt
@@ -0,0 +1,23 @@
+package com.example.barangay360_mobile
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
+
+class SplashActivity : AppCompatActivity() {
+
+    private val SPLASH_DISPLAY_LENGTH = 3000L // Duration in milliseconds
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            val mainIntent = Intent(this@SplashActivity, MainActivity::class.java)
+            startActivity(mainIntent)
+            finish()
+        }, SPLASH_DISPLAY_LENGTH)
+    }
+}
