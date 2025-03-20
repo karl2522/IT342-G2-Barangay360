2ea3796fa5a9900e9efd5cacf4c53843c4e16d9b
MikhailAg0ny <mikhailjpn@gmail.com>
2025-03-20 23:35:18 +0800

    Adding Fragment Profile TextView and Vector V1
    - Add Profile including the textview and the 3 vectors with scrollview
    - Not working only display the text
    - March 20, 2025
    - 11:34 PM

--- frontend_mobile/app/build.gradle.kts
+++ frontend_mobile/app/build.gradle.kts
@@ -83,6 +83,7 @@ dependencies {
     implementation ("androidx.core:core-ktx:1.9.0")
     implementation ("androidx.appcompat:appcompat:1.6.1")
     implementation ("com.google.android.material:material:1.8.0")
+    implementation ("de.hdodenhof:circleimageview:3.1.0")
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
